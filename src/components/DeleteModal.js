f148dff4e8c572b92080245edcd9ad875d4a7db4
Adithya Ajith <50308085+adithyajith925@users.noreply.github.com>
2021-10-04 14:51:23 -0400

    Items Load Properly

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 export default class DeleteModal extends Component {
     render() {
-        const { listKeyPair, hideDeleteListModalCallback } = this.props;
+        const { currentList, hideDeleteListModalCallback } = this.props;
         let name = "";
-        if (listKeyPair) {
-            name = listKeyPair.name;
+        if (currentList) {
+            name = currentList.name;
         }
         return (
             <div
@@ -14,7 +14,7 @@ export default class DeleteModal extends Component {
                 data-animation="slideInOutLeft">
                 <div className="modal-dialog">
                     <header className="dialog-header">
-                        Delete the {name} Top 5 List?
+                        Delete the Top 5 {name} List?
                     </header>
                     <div id="confirm-cancel-container">
                         <button
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,6 +2,19 @@ import React from "react";
 
 export default class Workspace extends React.Component {
     render() {
+        var item1 = "";
+        var item2 = "";
+        var item3 = "";
+        var item4 = "";
+        var item5 = "";
+        if (this.props.currentList) {
+            const {currentList} = this.props;
+            item1 = currentList.items[0];
+            item2 = currentList.items[1];
+            item3 = currentList.items[2];
+            item4 = currentList.items[3];
+            item5 = currentList.items[4];
+        }
         return (
             <div id="top5-workspace">
                 <div id="workspace-edit">
@@ -11,6 +24,13 @@ export default class Workspace extends React.Component {
                         <div className="item-number">3.</div>
                         <div className="item-number">4.</div>
                         <div className="item-number">5.</div>
+                    </div>
+                    <div id="edit-items">
+                        <div className="top5-item">{item1}</div>
+                        <div className="top5-item">{item2}</div>
+                        <div className="top5-item">{item3}</div>
+                        <div className="top5-item">{item4}</div>
+                        <div className="top5-item">{item5}</div>
                     </div>
                 </div>
             </div>
